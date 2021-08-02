1606d4b78c426ebaa17cbcda818494ff76386727
Hiren Reshamwala <hiren.reshamwala@gmail.com>
2021-08-02 12:59:08 +0530

    - enable/disable option added - radio group added

--- src/Field.vue
+++ src/Field.vue
@@ -1,28 +1,34 @@
 <template>
     <el-form-item v-if="type == 'textarea'" :label="label" :prop="property">
-        <el-input v-model="modelValue" v-on:input="$emit('update:modelValue', modelValue)" type="textarea" :autosize="{ minRows: 6 }"></el-input>
+        <el-input v-model="modelValue" v-on:input="$emit('update:modelValue', modelValue)" type="textarea" :autosize="{ minRows: 6 }" :disabled="disabled"></el-input>
     </el-form-item>
 
     <el-form-item v-else-if="type == 'checkbox'" :label="label" :prop="property">
-        <el-checkbox v-model="modelValue" @change="$emit('update:modelValue', modelValue)">{{ title }}</el-checkbox>
+        <el-checkbox v-model="modelValue" @change="$emit('update:modelValue', modelValue)" :disabled="disabled">{{ title }}</el-checkbox>
+    </el-form-item>
+
+    <el-form-item v-else-if="type == 'radio_group'" :label="label" :prop="property">
+        <el-radio-group v-model="modelValue" @change="$emit('update:modelValue', modelValue)" :disabled="disabled">
+            <el-radio v-for="(option, index) in options" :key="property+'-'+index" :label="option.value">{{ option.label }}</el-radio>
+        </el-radio-group>
     </el-form-item>
 
     <el-form-item v-else-if="type == 'switch'" :label="label" :prop="property">
-        <el-switch active-text="" inactive-text="" v-model="modelValue" v-on:input="$emit('update:modelValue', modelValue)"></el-switch>
+        <el-switch active-text="" inactive-text="" v-model="modelValue" v-on:input="$emit('update:modelValue', modelValue)" :disabled="disabled"></el-switch>
     </el-form-item>
 
     <el-form-item v-else-if="type == 'select'" :label="label" :prop="property">
-        <el-select v-model="modelValue" @input="$emit('update:modelValue', modelValue)" @change="$emit('update:modelValue', modelValue)">
+        <el-select v-model="modelValue" @input="$emit('update:modelValue', modelValue)" @change="$emit('update:modelValue', modelValue)" :disabled="disabled">
             <el-option v-for="(option, index) in options" :key="property+'-'+index" :label="option.label" :value="option.value"></el-option>
         </el-select>
     </el-form-item>
 
     <el-form-item v-else-if="type === 'password'" :label="label" :prop="property">
-        <el-input v-model="modelValue" @input="$emit('update:modelValue', modelValue)" type="password"></el-input>
+        <el-input v-model="modelValue" @input="$emit('update:modelValue', modelValue)" type="password" :disabled="disabled"></el-input>
     </el-form-item>
 
     <el-form-item v-else :label="label" :prop="property">
-        <el-input v-model="modelValue" @input="$emit('update:modelValue', modelValue)"></el-input>
+        <el-input v-model="modelValue" @input="$emit('update:modelValue', modelValue)" :disabled="disabled"></el-input>
     </el-form-item>
 </template>
 <script>
@@ -46,6 +52,10 @@ export default {
         label: {
             type: String,
             default: ''
+        },
+        disabled: {
+            type: Boolean,
+            default: false
         },
         options: {
             type: Array,
