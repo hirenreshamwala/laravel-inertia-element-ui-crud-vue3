272b1aada6c2b29cf0e5a678c9e13871bbf0b762
Hiren Reshamwala <hiren.reshamwala@gmail.com>
2022-12-15 23:32:14 +0530

    Added file extension while import

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-import ElCrudForm from './src/Form';
-import ElCrudList from './src/List';
+import ElCrudForm from './src/Form.vue';
+import ElCrudList from './src/List.vue';
+import ElField from './src/Field.vue';
 
 export {
     ElCrudForm,
     ElCrudList,
+    ElField
 }
